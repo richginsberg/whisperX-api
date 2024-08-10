d34a9b15b3752b148c2d11c023e7eee05b6cf146
Richard Ginsberg <machinez@yahoo.com>
2024-08-10 17:34:12 -0400

    Support JSON spacing in response if header 'X-Pretty-Json' is passed with a value of 'true'

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import whisperx
 import gc
 import torch
 import os
+import json
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from werkzeug.datastructures import FileStorage
@@ -20,6 +21,10 @@ ALLOWED_EXTENSIONS = {"mp3", "wav", "awb", "aac", "ogg", "oga", "m4a", "wma", "a
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Load models
+model = whisperx.load_model("large-v2", device, compute_type=compute_type)
+diarize_model = whisperx.DiarizationPipeline(use_auth_token=config.HF_TOKEN, device=device)
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -30,6 +35,8 @@ def transcribe():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
     file = request.files['file']
+    pretty_json = request.headers.get('X-Pretty-Json')
+    
     # if user does not select file, browser also
     # submit an empty part without filename
     if file.filename == '':
@@ -40,15 +47,20 @@ def transcribe():
         file.save(filepath)
         
         result = transcribe(filepath)
-        return jsonify(result)
+        if pretty_json and pretty_json.lower() == 'true':
+            # If the header is present and set to 'true', format JSON with 4 spaces indentation
+            response = jsonify(result)
+            response.data = json.dumps(response.json, indent=4)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+        else:
+            # Default behavior if the header is not present or set to something other than 'true'
+            return jsonify(result)
     else:
         return jsonify({'error': 'Invalid file type'}), 400
 
 def transcribe(audio_file):
-    # Load models
-    model = whisperx.load_model("large-v2", device, compute_type=compute_type)
-    diarize_model = whisperx.DiarizationPipeline(use_auth_token=config.HF_TOKEN, device=device)
-
+    global model, diarize_model
     # 1. Transcribe with original whisper (batched)
     audio = whisperx.load_audio(audio_file)
     result = model.transcribe(audio, batch_size=batch_size)
@@ -62,9 +74,6 @@ def transcribe(audio_file):
 
     # Return result
     result = whisperx.assign_word_speakers(diarize_segments, result)
-    del model, diarize_model
-    gc.collect
-    torch.cuda.empty_cache()
     return result
 
 if __name__ == '__main__':
